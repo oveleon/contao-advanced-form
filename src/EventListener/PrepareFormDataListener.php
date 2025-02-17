bc7f92941a5d749fa75bc256c02721a8bd6ca79f
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-02-17 16:33:31 +0100

    Handle the request files

--- src/EventListener/PrepareFormDataListener.php
+++ src/EventListener/PrepareFormDataListener.php
@@ -18,17 +18,19 @@ namespace Oveleon\ContaoAdvancedForm\EventListener;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\Form;
 use Oveleon\ContaoAdvancedForm\Service\FormPage\FormPageManagerFactory;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 #[AsHook('prepareFormData')]
 class PrepareFormDataListener
 {
     public function __construct(
         private readonly FormPageManagerFactory $formPageManager,
+        private readonly RequestStack $requestStack,
     ) {
     }
 
     // ToDo: Use the storage
-    public function __invoke(array &$submittedData, array &$labels, array $fields, Form $form): void
+    public function __invoke(array &$submittedData, array &$labels, array $fields, Form $form, array &$files): void
     {
         $manager = $this->formPageManager->getForForm($form);
 
@@ -40,23 +42,18 @@ class PrepareFormDataListener
         $manager->storeData($submittedData);
 
         // Submit form
-        if ($manager->isLastStep() && $_POST['pageSwitch'] === 'continue')
+        if ($manager->isLastStep() && 'continue' === $this->requestStack->getCurrentRequest()?->get('pageSwitch'))
         {
             $data = $manager->getDataOfAllSteps();
 
             $submittedData = $data['submitted'];
             $labels = $data['labels'];
-            $_SESSION['FILES'] = $data['files'];
-
-            $_POST = $submittedData;
-
-            $_SESSION['FORM_DATA'] = $submittedData;
+            $files = $data['files'];
 
             $manager->resetData();
 
             return;
         }
-        $_SESSION['FORM_DATA'] = [];
 
         $manager->redirectToStep($manager, $manager->getNextStep());
     }
